49d2f3e7705a9d1f6ee6b89376bcd3fb6018eb34
= <tim@creationix.com>
2015-05-29 00:15:50 -0500

    Add server-side to ide over websocket

--- main.lua
+++ main.lua
@@ -1,11 +1,45 @@
 
 websocket.createServer(80, function (socket)
+  local data
+  node.output(function (msg)
+    return socket.send(msg, 1)
+  end, 1)
+  print("New websocket client connected")
+
   function socket.onmessage(payload, opcode)
     if opcode == 1 then
-      print(payload)
-    else
-      print("<" .. crypto.toHex(payload) .. ">")
+      if payload == "ls" then
+        local list = file.list()
+        local lines = {}
+        for k, v in pairs(list) do
+          lines[#lines + 1] = k .. "\0" .. v
+        end
+        socket.send(table.concat(lines, "\0"), 2)
+        return
+      end
+      local command, name = payload:match("^([a-z]+):(.*)$")
+      if command == "load" then
+        file.open(name, "r")
+        socket.send(file.read(), 2)
+        file.close()
+      elseif command == "save" then
+        file.open(name, "w")
+        file.write(data)
+        data = nil
+        file.close()
+      elseif command == "compile" then
+        node.compile(name)
+      elseif command == "run" then
+        dofile(name)
+      elseif command == "eval" then 
+        local fn = loadstring(data, name)
+        data = nil
+        fn()
+      else
+        print("Invalid command: " .. command)
+      end
+    elseif opcode == 2 then
+      data = payload
     end
-    socket.send(payload, opcode)
   end
 end)
